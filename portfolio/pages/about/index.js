ca99e4321ef45e3488806a966d08ebf501463eaa
VirkUsman <usmanvirk11@gmail.com>
2023-09-08 05:09:27 +0200

    Git hub Updated

--- portfolio/pages/about/index.js
+++ portfolio/pages/about/index.js
@@ -111,11 +111,20 @@ const About = () => {
         <Avatar2 />
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
-        <div>text</div>
-        <div >
-        <div className="flex gap-x-4" >
+        <div className="flex-1 flex flex-col justify-center">text</div>
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+        <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4" >
             {aboutData.map((item, itemIndex) => {
               return <div key={itemIndex} className= { ` ${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`} onClick={() => setIndex(itemIndex)} >{item.title}</div>
+            })}
+          </div>
+          <div>
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div key={itemIndex}>
+                  <div>{item.title}</div>
+                </div>
+              )
             })}
           </div>
           </div>
